8d4e31230ce211375ce21c6438654496992c0939
Przemek Trepka <hi@przemektrepka.pl>
2019-06-21 08:48:46 +0200

    Fix footer

--- footer.php
+++ footer.php
@@ -11,8 +11,21 @@
 
 		</div>
 
+  <footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="container">
+      <?php
+      /**
+       * Functions hooked in to storefront_footer action
+       *
+       * @hooked storefront_footer_widgets - 10
+       * @hooked storefront_footer_nav         - 20
+       */
+      do_action( 'storefront_child_footer' );
+      ?>
+    </div>
+  </footer><!-- #colophon -->
+
   <?php wp_footer(); ?>
-  <?php do_action( 'storefront_child_footer' ); ?>
   <?php do_action( 'storefront_child__footer_fixed_overlay' ); ?>
 
 	<?php do_action( 'storefront_after_footer' ); ?>
