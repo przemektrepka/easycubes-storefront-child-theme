29781e97b5eb8399473fe18ec225c7f3f4a509c1
Przemek Trepka <hi@przemektrepka.pl>
2019-06-21 08:56:17 +0200

    Revert

--- inc/childtheme-overwrites.php
+++ inc/childtheme-overwrites.php
@@ -6,7 +6,6 @@
 
 
 // Remove stuff
-remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
 
--- inc/childtheme-single-product-extension.php
+++ inc/childtheme-single-product-extension.php
@@ -1,41 +1,12 @@
 <?php
 
-/**
- * Rework price + add to cart layout
- */
-add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_before_price', 9 );
-add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_after_price', 11 );
-add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_after_atc', 31 );
-
-function woocommerce_template_single_before_price() {
-  ?>
-  <div class="row align-items-center price-n-cart">
-    <div class="col">
-  <?php
-}
-
-function woocommerce_template_single_after_price() {
-  ?>
-    </div>
-    <div class="col">
-  <?php
-}
-
-function woocommerce_template_single_after_atc() {
-  ?>
-    </div>
-  </div>
-  <?php
-}
-
-/**
- * Add attribus to product summary
- */
-add_action( 'woocommerce_single_product_summary', 'show_attr_in_prod_summary', 35 );
+add_action( 'woocommerce_single_product_summary', 'show_attr_in_prod_summary', 31 );
 
 function show_attr_in_prod_summary() {
+
   $tabs = apply_filters( 'woocommerce_product_tabs', array() );
   if ( ! empty( $tabs ) ) : ?>
+
     <div class="single-product--extra-info">
       <?php foreach ( $tabs as $key => $tab ) : ?>
         <div class="panel" id="tab-<?php echo esc_attr( $key ); ?>" role="tabpanel" aria-labelledby="tab-title-<?php echo esc_attr( $key ); ?>">
@@ -43,31 +14,17 @@ function show_attr_in_prod_summary() {
         </div>
       <?php endforeach; ?>
     </div>
+
   <?php endif; ?>
+
   <?php
 }
 
 
 /**
- * Change tab headings
- */
-add_filter( 'woocommerce_product_additional_information_heading', 'change_product_additional_information_tab_heading', 10, 1 );
-function change_product_additional_information_tab_heading( $title ) {
-  // $title = 'Product data';
-  $title = '';
-  return $title;
-}
-
-add_filter( 'woocommerce_product_description_heading', 'change_product_description_tab_heading', 10, 1 );
-function change_product_description_tab_heading( $title ) {
-  $title = '';
-  return $title;
-}
-
-/**
  * Add a custom product data tab
  */
-add_filter( 'woocommerce_product_tabs', 'edit_product_tabs', 98 );
+add_filter( 'woocommerce_product_tabs', 'edit_product_tabs' );
 function edit_product_tabs( $tabs ) {
 
   $tabs['description']['title'] = 'More Information';   // Rename the description tab
@@ -76,6 +33,7 @@ function edit_product_tabs( $tabs ) {
 // Adds the new tab
   $tabs['product_meta_tab'] = array(
     'title'   => __( 'New Product Tab', 'woocommerce' ),
+    'priority'  => 50,
     'callback'  => 'product_meta_tab_content',
   );
   return $tabs;
@@ -99,18 +57,4 @@ function product_meta_tab_content() {
   </tbody>
 </table>
 <?php
-}
-
-/**
- * Reorder product data tabs
- */
-add_filter( 'woocommerce_product_tabs', 'woo_reorder_tabs', 98 );
-function woo_reorder_tabs( $tabs ) {
-
-  $tabs['description']['priority'] = 5;
-  $tabs['additional_information']['priority'] = 10;
-  $tabs['product_meta_tab']['priority'] = 15;
-  $tabs['reviews']['priority'] = 50;     // Reviews last
-
-  return $tabs;
 }
